6f79659cfaf528c8a29ea1383ca9eebbc0786b6a
Ai-Kam <Ai-Kam@github.com>
2024-05-24 08:35:17 +0900

    20240524-00

--- 05_genetic_algorithms/ga.py
+++ 05_genetic_algorithms/ga.py
@@ -1,6 +1,6 @@
 import random
 
-target_sentence = "Hello, how are you?"
+target_sentence = "I found what I was looking for!"
 gene_pool="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?., "
 
 population_size = 10
@@ -12,38 +12,55 @@ def calculate_fitness(chromosome):
             fitness += 1
     return fitness
 
-
 def generate_chromosome(length):
     return ''.join(random.choice(gene_pool) for _ in range(length))
 
-def mutate(chromosome):
+def mutate(chromosome, prob):
     gene = list(chromosome)
-    index = random.randint(0, len(gene) - 1)
+    sum_prob = sum(prob)
+    choosen = random.randint(0, sum_prob-1)
+    for i in range(len(prob)):
+        choosen -= prob[i]
+        if choosen < 0:
+            index = i
+            break
+    # index is given ramdomly, proportional to each index's probability.
     gene[index] = random.choice(gene_pool)
-    return ''.join(gene)
+    return ''.join(gene), index
 
 if __name__ == "__main__":
-    population = []
-    for _ in range(population_size):
-        population.append(generate_chromosome(len(target_sentence)))
-
-    population_fitness = []
-    for chromosome in population:
-        population_fitness.append(calculate_fitness(chromosome))
+    population = [generate_chromosome(len(target_sentence)) for _ in range(population_size)]
+    population_fitness = [calculate_fitness(chromosome) for chromosome in population]
+    probability_limit = 100
+    probability_penalty = 100
+    probability = [[probability_limit for _ in range(len(target_sentence))] for _ in range(population_size)]
 
     for generation in range(10000):
         parent_index = population_fitness.index(max(population_fitness))
         parent = population[parent_index]
-        child = mutate(parent)
+        child, changed_index = mutate(parent, probability[parent_index])
 
-        print(f"Generation {generation}: {parent} -> {child}")
 
         child_fitness = calculate_fitness(child)
-        print(f"Child Fitness: {child_fitness}")
+        print(f"Generation {generation}: {parent} -> {child}, Child Fitness: {child_fitness}")
 
-        index_to_replace = population_fitness.index(min(population_fitness))
-        population[index_to_replace] = child
-        population_fitness[index_to_replace] = child_fitness
+        child_probability = probability[parent_index]
+        if child_fitness < population_fitness[parent_index]:
+            # If child is worce than parent, that change at the index is not good.
+            probability[parent_index][changed_index] = max(child_probability[changed_index]-probability_penalty, 0)
+        else:
+            if child_fitness > population_fitness[parent_index]:
+                # If child is better than parent, there is less chance of improvement.
+                child_probability[changed_index] = max(child_probability[changed_index]-probability_penalty, 0)
+            index_to_replace = population_fitness.index(min(population_fitness))
+            population[index_to_replace] = child
+            population_fitness[index_to_replace] = child_fitness
+            probability[index_to_replace] = child_probability
+            # Make the limitation of probability milder if there is no improvement.
+            probability = [[min(x+1, probability_limit) for x in y] for y in probability]
+        print(f"Current Population: {population}")
+        print(f"Current Population Fitness: {population_fitness}")
+        #print(f"Current Probability: {probability}")
 
         if child_fitness == len(target_sentence):
             print(f"Solution found in generation {generation}")
